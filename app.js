8d69e53392cae1cedf7fcffc54482df4dfe718bb
Joshua Green <josh@joshuas-mbp.home>
2018-06-22 22:49:52 -0500

    added database dependencies, setup connection and db error handling'

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const bodyParser = require('body-parser');
 const cors = require('cors'); 
 const passport = require('passport'); 
 const mongoose = require('mongoose'); 
+const config = require('./config/database');
+
+//add database
+mongoose.connect(config.database);
+
+//test if connected to database
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database: '+config.database)
+});
+
+//error if can't connect to db
+mongoose.connection.on('error', (err) => {
+    console.log('Database error: '+err)
+});
 
 const app = express();
 const users = require('./routes/users');
@@ -13,6 +27,9 @@ const port = 3000;
 
 //CORS middleware
 app.use(cors());
+
+//Set static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Body parser Middleware
 app.use(bodyParser.json());
