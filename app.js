ce161e2e7649d280b8923d846e5863fbca451550
Joshua Green <josh@joshuas-mbp.home>
2018-06-22 22:40:46 -0500

    added routes and app config

--- app.js
+++ app.js
@@ -6,13 +6,25 @@ const passport = require('passport');
 const mongoose = require('mongoose'); 
 
 const app = express();
+const users = require('./routes/users');
 
+//Port number
 const port = 3000;
 
+//CORS middleware
+app.use(cors());
+
+//Body parser Middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+//Index Route 
 app.get('/', (req,res) => {
     res.send('Invalid endpoint');
 });
 
+//Start server
 app.listen(port, () => {
     console.log('Server started on port '+port);
 });
